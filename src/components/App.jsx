44f7762e7862aa213cdc16cbefd7372a48b7c4df
Virac David AUK <auk.david@gmail.com>
2021-04-01 20:50:22 +0200

    Add counter

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,32 +8,45 @@ class App extends Component {
     super(props)
 
     this.selectTab = this.selectTab.bind(this)
-    this.refreshCounter = this.refreshCounter.bind(this)
+    this.count = this.count.bind(this)
+    this.recount = this.recount.bind(this)
 
     this.state = {
       currentTab: null,
       boxes: [],
-      max: '-'
+      pokemonCaught: '-',
+      max: '-',
     }
   }
 
   selectTab(event) {
     const { tab: currentTab, src } = event.target.dataset
-    this.setState({ boxes: [], currentTab })
+    this.setState({ currentTab, boxes: [], pokemonCaught: '-', max: '-' })
     getBoxes(src).then(({ boxes, max }) => {
-      this.setState({ boxes, max })
+      const pokemonCaught = this.count(boxes)
+      this.setState({ boxes, pokemonCaught, max })
     })
   }
 
-  refreshCounter() {
-    this.forceUpdate()
+  count(boxes) {
+    let counter = 0
+    boxes.forEach((pokemons) => {
+      pokemons.forEach((name) => {
+        if (localStorage.getItem(name)) counter++
+      })
+    })
+    return counter
+  }
+
+  recount(event) {
+    this.setState({ pokemonCaught: this.count(this.state.boxes) })
   }
 
   render() {
-    const { currentTab, boxes, max } = this.state
+    const { currentTab, boxes, pokemonCaught, max } = this.state
 
     return (
-      <div className="App" onClick={this.refreshCounter}>
+      <div className="App">
         <header className="Header">
           <div className="title">
             <img src="./images/pokeball.png" />
@@ -41,12 +54,12 @@ class App extends Component {
           </div>
           <Navigation onClick={this.selectTab} currentTab={currentTab} />
         </header>
-        <main>
+        <main onClick={this.recount}>
           {boxes.map((pokemons, key) => <Box key={key} num={key + 1} pokemons={pokemons} />)}
         </main>
         <footer className="Footer">
           <div>Version 1.2.0</div>
-          <div>{localStorage.length}/{max}</div>
+          <div>{pokemonCaught}/{max}</div>
         </footer>
       </div>
     )
