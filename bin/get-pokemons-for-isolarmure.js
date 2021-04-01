4090055fd7db2281c145b749552e9c9003699ae8
Virac David AUK <auk.david@gmail.com>
2021-04-01 09:34:18 +0200

    Fetch Isolarmure pokemons

---
+++ bin/get-pokemons-for-isolarmure.js
@@ -0,0 +1,28 @@
+const fsp = require('fs').promises
+const path = require('path')
+const request = require('request')
+const { JSDOM } = require('jsdom')
+
+const DEST = path.resolve(__dirname, '../data/galar-isolarmure.json')
+const URL = 'https://www.jeuxvideo.com/wikis-soluce-astuces/1247746/liste-des-211-pokemon-du-dlc-isolarmure.htm'
+
+request({ method: 'GET', url: URL }, (err, res, body) => {
+  const pokemons = []
+  const doc = (new JSDOM(body)).window.document
+
+  const table = doc.querySelector('table')
+
+  const rows = table.querySelectorAll('tbody tr')
+
+  rows.forEach((row, i) => {
+    const columns = row.querySelectorAll('td')
+    const src = columns[3].querySelector('img').src
+    const name = columns[2].textContent
+    const pokemon = { src, name }
+    pokemons.push(pokemon)
+  })
+
+  fsp.writeFile(DEST, JSON.stringify(pokemons))
+    .then(() => console.log('Succeed!'))
+    .catch((e) => console.log('Failed :(', e))
+})
