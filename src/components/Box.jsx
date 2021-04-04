7f8761dbfe3e93ac4a890fbc4fa682d56ed10e3e
Virac David AUK <auk.david@gmail.com>
2021-04-04 23:39:35 +0200

    Add counter to each box (#5)

--- src/components/Box.jsx
+++ src/components/Box.jsx
@@ -5,19 +5,44 @@ class Box extends Component {
   constructor(props) {
     super(props)
 
+    this.countCurrent = this.countCurrent.bind(this)
     this.toggle = this.toggle.bind(this)
+
+    this.state = {
+      current: 0,
+    }
+  }
+
+  countCurrent() {
+    let i = 0
+    const { pokemons } = this.props
+
+    pokemons.forEach((name) => {
+      if (localStorage.getItem(name)) {
+        i++
+      }
+    })
+
+    return i
   }
 
   toggle(event) {
-    if (event.target.tagName !== 'H1') return
-    event.currentTarget.classList.toggle('hidden')
+    if (event.target.tagName === 'H1') {
+      event.currentTarget.classList.toggle('hidden')
+    }
+    this.setState({ current: this.countCurrent() })
+  }
+
+  componentDidMount() {
+    this.setState({ current: this.countCurrent() })
   }
 
   render() {
     const { num, pokemons } = this.props
+    const { current } = this.state
     return (
-      <section className="Box" onClick={this.toggle}>
-        <h1>Boîte n°{num}</h1>
+      <section className="Box hidden" onClick={this.toggle}>
+        <h1>Boîte n°{num} ({current}/30)</h1>
         <div className="Pokemons">
           {pokemons.map((name, key) => <Pokemon key={key} name={name} />)}
         </div>
