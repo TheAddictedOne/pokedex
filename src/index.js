81720b87c291b20a16120c86f05545edef8ad228
Virac David AUK <auk.david@gmail.com>
2021-04-08 09:04:03 +0200

    Add animation

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import 'src/index.scss'
 import { render } from 'react-dom'
 import Pokedex from 'src/components/Pokedex.jsx'
 
-render(<Pokedex />, document.getElementById('Root'))
+render(<Pokedex animate />, document.getElementById('Root'))
