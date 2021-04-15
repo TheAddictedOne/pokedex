252b1d42348f03eec987fb5a5f1cd62c11786373
Virac David AUK <auk.david@gmail.com>
2021-04-15 13:22:50 +0200

    Delete Tab component

--- src/components/Tab.jsx
+++
@@ -1,4 +0,0 @@
-export default function({ currentTab, tab, src }) {
-  const className = currentTab === tab ? 'selected' : ''
-  return <div className={className} data-tab={tab} data-src={src}>{tab}</div>
-}
